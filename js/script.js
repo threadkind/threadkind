62da78f00bf1b7ee38e4ddd38f563cff6e8f8951
Amy <amy.otoole.uk@gmail.com>
2016-12-01 00:04:17 -0600

    Recipe type toggle

--- js/script.js
+++ js/script.js
@@ -6,8 +6,15 @@ $('.hamburger').on('click', function () {
 $('.menu').toggleClass('open');
 });
 
-//PRINT RECIPE FUNCTION
+//FOOD CATEGORIES EXPAND
 
+$('.recipegroup').on('click', function(){
+var thumbToggle = $(this).find('img');
+console.log($(thumbToggle).attr('src'));
+$(thumbToggle).toggleClass('recipeclosed', 500);
+});
+
+//PRINT RECIPE FUNCTION
 
 $('.printMe').click(function() {
     var mywindow = window.open('', 'PRINT');
@@ -21,5 +28,6 @@ $('.printMe').click(function() {
     mywindow.close();
     return false;
 });
+
 
 }); // DOC READY CLOSE
