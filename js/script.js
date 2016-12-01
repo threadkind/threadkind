458a6e009e9b06bd1e214ca199fd35b0d82a6ae6
Amy <amy.otoole.uk@gmail.com>
2016-11-30 23:06:36 -0600

    Update print function

--- js/script.js
+++ js/script.js
@@ -8,27 +8,6 @@ $('.menu').toggleClass('open');
 
 //PRINT RECIPE FUNCTION
 
-function PrintElem(elem)
-    {
-      var mywindow = window.open('', 'PRINT', 'height=400,width=600');
-
-
-        mywindow.document.write('<html><head><title>' + document.title  + '</title>');
-
-        mywindow.document.write('</head><body >');
-      mywindow.document.write('<h1>' + document.title  + '</h1>');
-        mywindow.document.write(document.getElementById(elem).innerHTML);
-        mywindow.document.write('</body></html>');
-
-        mywindow.document.close(); // necessary for IE >= 10
-        mywindow.focus(); // necessary for IE >= 10*/
-
-        mywindow.print();
-        mywindow.close();
-
-        return true;
-
-        }
 
 $('.printMe').click(function() {
     var mywindow = window.open('', 'PRINT');
@@ -40,7 +19,6 @@ $('.printMe').click(function() {
 
     mywindow.print();
     mywindow.close();
-    window.location.reload();
     return false;
 });
 
